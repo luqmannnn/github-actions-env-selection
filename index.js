ee6a859a46c56908de31597fb4b2b75b59d1bbc8
iuolxvpq <luqmannurhakimt@gmail.com>
2024-05-05 22:04:42 +0800

    update changes of dockerfile

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-console.log('Loading function');
+'use strict';
 
-export const handler = async (event, context) => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify(
-            {
-                message: "Hello, World!",
-            },
-          null,
-          2
-        ),
-    };
-};
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+const OS = require('os');
+const ENV = 'DEV';
+
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+    res.statusCode = 200;
+    const msg = 'Hello from Node 3!';
+    res.send(msg);
+});
+
+app.get('/test', (req, res) => {
+    res.statusCode = 200;
+    const msg = 'Hello from /test Node!';
+    res.send(msg);
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
